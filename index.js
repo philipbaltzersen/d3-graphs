17a82393a59b22ddf6b544548c0040e24e8a8b4a
Philip Holm <philipbaltzersen@gmail.com>
2024-06-19 19:28:12 +0200

    Add a square canvas

---
+++ index.js
@@ -0,0 +1,10 @@
+const width = 450;
+const height = 300;
+const margin = {top: 20, right: 30, bottom: 30, left: 40}
+
+const svg = d3.select("body")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .style("border", "1px solid black");
+
